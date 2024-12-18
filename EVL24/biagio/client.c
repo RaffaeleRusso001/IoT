5a384447191c444bc5d5729d7f1499a7fb5da430
RaffaeleRusso <russoraffaele031@gmail.com>
2024-12-18 09:27:31 +0100

    biagio client
    biagio client

--- EVL24/biagio/client.c
+++ EVL24/biagio/client.c
@@ -2,6 +2,7 @@
 #include "net/routing/routing.h"
 #include "net/netstack.h"
 #include "net/ipv6/simple-udp.h"
+#include "net/routing/rpl-lite/rpl.h"  // Include per accedere alle funzioni RPL
 #include "sys/log.h"
 
 #define LOG_MODULE "Client"
@@ -12,41 +13,51 @@
 
 static struct simple_udp_connection udp_conn;
 
+/*---------------------------------------------------------------------------*/
 PROCESS(client_process, "UDP Client");
 AUTOSTART_PROCESSES(&client_process);
+/*---------------------------------------------------------------------------*/
 
 PROCESS_THREAD(client_process, ev, data)
 {
   static struct etimer timer;
-  static uint16_t node_id = 1; // ID univoco del nodo
+  static uint16_t node_id = 0; // ID univoco del nodo
   uip_ipaddr_t root_ip;
+  uip_ipaddr_t parent_ip;
 
   PROCESS_BEGIN();
 
-  // Registrazione connessione UDP
+  LOG_INFO("UDP Client process started\n");
+
+  /* Registra la connessione UDP */
   simple_udp_register(&udp_conn, UDP_CLIENT_PORT, NULL, UDP_SERVER_PORT, NULL);
 
-  // Imposta il timer per 60 secondi
+  /* Imposta il timer per inviare ogni minuto */
   etimer_set(&timer, CLOCK_SECOND * 60);
 
   while (1) {
     PROCESS_WAIT_EVENT_UNTIL(etimer_expired(&timer));
 
-    if (NETSTACK_ROUTING.node_is_reachable() &&
-        NETSTACK_ROUTING.get_root_ipaddr(&root_ip)) {
-      // Ottieni il next hop (verifica che l'API esista nella tua versione)
-      const linkaddr_t *parent = NETSTACK_ROUTING.get_nexthop();
-      uint16_t parent_id = parent ? parent->u8[7] : 0; // Usa l'ultima parte dell'indirizzo come ID
+    /* Verifica se il nodo root è raggiungibile */
+    if (NETSTACK_ROUTING.node_is_reachable() && NETSTACK_ROUTING.get_root_ipaddr(&root_ip)) {
+      /* Recupera l'indirizzo IPv6 del parent preferito */
+      rpl_dag_t *dag = rpl_get_any_dag();
+      if (dag && dag->preferred_parent) {
+        rpl_parent_get_ipaddr(dag->preferred_parent, &parent_ip);
 
-      uint16_t data[2] = {node_id, parent_id};
+        /* Invia ID nodo e indirizzo parent al root */
+        uint16_t data[2] = {node_id, parent_ip.u8[15]}; // Invio node_id e ultimo byte dell'IP parent
+        simple_udp_sendto(&udp_conn, data, sizeof(data), &root_ip);
 
-      simple_udp_sendto(&udp_conn, data, sizeof(data), &root_ip);
-      LOG_INFO("Sent node %u, parent %u\n", node_id, parent_id);
+        LOG_INFO("Sent: node %u, parent IP ...:%02x\n", node_id, parent_ip.u8[15]);
+      } else {
+        LOG_INFO("No preferred parent found\n");
+      }
     } else {
       LOG_INFO("Root not reachable\n");
     }
 
-    etimer_reset(&timer);
+    etimer_reset(&timer); // Reset del timer per il prossimo invio
   }
 
   PROCESS_END();
