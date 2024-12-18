c7ffa7a3aca81eaf97fa66b018d369c3414ba173
RaffaeleRusso <russoraffaele031@gmail.com>
2024-12-18 09:38:55 +0100

    attilio
    attilio

--- EVL24/attilio_semplice/semplice.c
+++ EVL24/attilio_semplice/semplice.c
@@ -34,6 +34,9 @@ AUTOSTART_PROCESSES(&rpl_monitor_process);
 /* Function to configure the RPL root */
 static void configure_rpl_root() {
   uip_ipaddr_t root_ip;
+  rpl_prefix_t prefix;
+
+  /* Create a link-local address for the root */
   uip_create_linklocal_prefix(&root_ip);
   uip_ds6_set_addr_iid(&root_ip, &uip_lladdr);
 
@@ -42,8 +45,16 @@ static void configure_rpl_root() {
     return;
   }
 
-  rpl_set_root(RPL_DEFAULT_INSTANCE, &root_ip);
-  rpl_set_prefix(RPL_DEFAULT_INSTANCE, &root_ip, 64);
+  /* Set the RPL DAG root and configure the prefix */
+  rpl_dag_root_init();
+  uip_ip6addr_copy(&prefix.prefix, &root_ip);
+  prefix.length = 64;
+
+  if (!rpl_set_prefix(&prefix)) {
+    LOG_ERR("Failed to set RPL prefix\n");
+    return;
+  }
+
   LOG_INFO("RPL Root configured with address ");
   LOG_INFO_6ADDR(&root_ip);
   LOG_INFO_("\n");
@@ -97,43 +108,4 @@ static void udp_rx_callback(struct simple_udp_connection *c,
          const uip_ipaddr_t *sender_addr, uint16_t sender_port,
          const uip_ipaddr_t *receiver_addr, uint16_t receiver_port,
          const uint8_t *data, uint16_t datalen) {
-  if (datalen != sizeof(uip_ipaddr_t)) {
-    return;
-  }
-
-  uip_ipaddr_t parent_ip;
-  memcpy(&parent_ip, data, sizeof(uip_ipaddr_t));
-  update_client(sender_addr, &parent_ip);
-}
-
-/*---------------------------------------------------------------------------*/
-PROCESS_THREAD(rpl_monitor_process, ev, data) {
-  static struct etimer timer;
-
-  PROCESS_BEGIN();
-
-  /* Initialize clients */
-  for (int i = 0; i < MAX_CLIENTS; i++) {
-    clients[i].last_update = 0;
-  }
-
-  /* Configure RPL root */
-  configure_rpl_root();
-
-  /* Initialize UDP connection */
-  simple_udp_register(&udp_conn, UDP_SERVER_PORT, NULL,
-                      UDP_CLIENT_PORT, udp_rx_callback);
-
-  etimer_set(&timer, REPORT_INTERVAL);
-
-  while (1) {
-    PROCESS_WAIT_EVENT_UNTIL(etimer_expired(&timer));
-
-    current_time++;
-    remove_inactive_clients();
-    print_clients();
-    etimer_reset(&timer);
-  }
-
-  PROCESS_END();
-}
+  if (
